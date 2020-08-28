e913d0c14f81321928ea13b0bbc9230094fb0467
Kenneth Bongort <kbongort@gmail.com>
2020-08-27 18:37:55 -0700

    Fix layout so lights are centered in the view.

--- CAMEL/AppDelegate.swift
+++ CAMEL/AppDelegate.swift
@@ -5,16 +5,16 @@ import GRPC
 let points: [Int:CGPoint] = [
   6:  CGPoint(x: 0,    y: 0.22),
   1:  CGPoint(x: 0,    y: 0.78),
-  5:  CGPoint(x: 0.08, y: 0),
-  0:  CGPoint(x: 0.08, y: 0.5),
-  2:  CGPoint(x: 0.08, y: 1),
-  4:  CGPoint(x: 0.15, y: 0.22),
-  3:  CGPoint(x: 0.15, y: 0.78),
-  10: CGPoint(x: 0.85, y: 0.22),
-  11: CGPoint(x: 0.85, y: 0.78),
-  9:  CGPoint(x: 0.92, y: 0),
-  7:  CGPoint(x: 0.92, y: 0.5),
-  12: CGPoint(x: 0.92, y: 1),
+  5:  CGPoint(x: 0.07, y: 0),
+  0:  CGPoint(x: 0.07, y: 0.5),
+  2:  CGPoint(x: 0.07, y: 1),
+  4:  CGPoint(x: 0.14, y: 0.22),
+  3:  CGPoint(x: 0.14, y: 0.78),
+  10: CGPoint(x: 0.86, y: 0.22),
+  11: CGPoint(x: 0.86, y: 0.78),
+  9:  CGPoint(x: 0.93, y: 0),
+  7:  CGPoint(x: 0.93, y: 0.5),
+  12: CGPoint(x: 0.93, y: 1),
   8:  CGPoint(x: 1,    y: 0.22),
   13: CGPoint(x: 1,    y: 0.78),
 ]
@@ -27,26 +27,16 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
   @IBOutlet weak var window: NSWindow!
   @IBOutlet weak var view: NSView!
-  var lightViews: [LightView] = []
+  var monitorView: MonitorView?
 
   func applicationDidFinishLaunching(_ aNotification: Notification) {
     // Insert code here to initialize your application
-
-    addLightViews()
+    monitorView = MonitorView(frame: view.frame)
+    view.addSubview(monitorView!)
+    monitorView!.autoresizingMask = [.width, .height]
 
     group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
     connect(group: group!)
-  }
-
-  func addLightViews() {
-    for (_, position) in points.sorted(by: { $0.key < $1.key }) {
-      let origin = CGPoint(x: position.x * 400, y: position.y * 60)
-      let lightView = LightView(frame: NSRect(x: origin.x, y: origin.y, width: 20, height: 20))
-      lightView.layer?.cornerRadius = 10
-      lightView.layer?.backgroundColor = .black
-      view.addSubview(lightView)
-      lightViews.append(lightView)
-    }
   }
 
   func applicationWillTerminate(_ aNotification: Notification) {
@@ -70,29 +60,22 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     let options = CallOptions()
     let call = client.lightStateStream(req, callOptions: options) { (lightState) in
       DispatchQueue.main.async {
-        self.updateLightState(lightState)
+        self.monitorView?.updateLightState(lightState)
       }
     }
     call.status.whenComplete { (result) in
       print("done with rpc \(result)")
       DispatchQueue.main.async {
         // Restart stream on disconnect.
-        self.startStreaming()
-      }
-    }
-  }
-
-  func updateLightState(_ lightState: RPC_LightState) {
-    for (i, color) in lightState.lightColors.enumerated() {
-      if i < lightViews.count {
-        lightViews[i].layer?.backgroundColor = CGColor(red: CGFloat(color.red) / 255.0, green: CGFloat(color.green) / 255.0, blue: CGFloat(color.blue) / 255.0, alpha: 1)
-        lightViews[i].setNeedsDisplay(lightViews[i].frame)
+        if let group = self.group {
+          self.connect(group: group)
+        }
       }
     }
   }
 }
 
-class LightView: NSView {
+class LayerBackedView: NSView {
   override init(frame frameRect: NSRect) {
     super.init(frame: frameRect)
     self.wantsLayer = true
@@ -100,5 +83,62 @@ class LightView: NSView {
 
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+}
+
+class MonitorView: LayerBackedView {
+
+  var lightContainer = LayerBackedView()
+  var lightViews: [NSView] = []
+
+  override init(frame frameRect: NSRect) {
+    super.init(frame: frameRect)
+    self.wantsLayer = true
+    self.layer?.backgroundColor = .black
+    setUp()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  func setUp() {
+    lightContainer.frame = CGRect(origin: .zero, size: .init(width: 420, height: 80))
+    lightContainer.autoresizingMask = [.maxXMargin, .maxYMargin, .minXMargin, .minYMargin]
+    lightContainer.translatesAutoresizingMaskIntoConstraints = true
+    addSubview(lightContainer)
+
+    // Create & add views for each light.
+    for (_, position) in points.sorted(by: { $0.key < $1.key }) {
+      let origin = CGPoint(x: position.x * (lightContainer.frame.width - 20), y: position.y * (lightContainer.frame.height - 20))
+      let lightView = LayerBackedView(frame: NSRect(x: origin.x, y: origin.y, width: 20, height: 20))
+      lightView.layer?.cornerRadius = 10
+      lightView.layer?.backgroundColor = .black
+      lightViews.append(lightView)
+      lightContainer.addSubview(lightView)
+    }
+
+    // Have to dispatch async to get layout to happen.
+    DispatchQueue.main.async {
+      self.layout()
+    }
+  }
+
+  override func layout() {
+    super.layout()
+    lightContainer.layer?.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+    lightContainer.layer?.position = CGPoint(x: frame.midX, y: frame.midY)
+  }
+
+  func updateLightState(_ lightState: RPC_LightState) {
+    for (i, color) in lightState.lightColors.enumerated() {
+      if i < lightViews.count {
+        lightViews[i].layer?.backgroundColor = CGColor(red: CGFloat(color.red) / 255.0,
+                                                       green: CGFloat(color.green) / 255.0,
+                                                       blue: CGFloat(color.blue) / 255.0,
+                                                       alpha: 1)
+        lightViews[i].setNeedsDisplay(lightViews[i].frame)
+      }
+    }
   }
 }
